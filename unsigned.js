e8d5047df9180645e36e2f65df0343d6fa870aaa
Dmitry Tsvettsikh <dmitrycvet@gmail.com>
2017-08-21 01:07:54 +0500

    a bit improve code style
    add .gitignore
    
    add "repository" field to package.json

--- unsigned.js
+++ unsigned.js
@@ -2,56 +2,64 @@ const Bn = require('bn.js')
 const Stream = require('./stream.js')
 
 module.exports = {
-  read: (stream) => {
-    return module.exports
-      .readBn(stream)
-      .toString()
-  },
-  readBn: (stream) => {
-    const num = new Bn(0)
-    let shift = 0
-    let byt
-    while (true) {
-      byt = stream.read(1)[0]
-      num.ior(new Bn(byt & 0x7f).shln(shift))
-      if (byt >> 7 === 0) {
-        break
-      } else {
-        shift += 7
-      }
+  encode,
+  decode,
+  read,
+  readBn,
+  write
+}
+
+function read (stream) {
+  return readBn(stream).toString()
+}
+
+function readBn (stream) {
+  const num = new Bn(0)
+  let shift = 0
+  let byt
+  while (true) {
+    byt = stream.read(1)[0]
+    num.ior(new Bn(byt & 0x7f).shln(shift))
+    if (byt >> 7 === 0) {
+      break
+    } else {
+      shift += 7
     }
-    return num
-  },
-  write: (json, stream) => {
-    const num = new Bn(json)
-    while (true) {
-      const i = num.maskn(7).toNumber()
-      num.ishrn(7)
-      if (num.isZero()) {
-        stream.write([i])
-        break
-      } else {
-        stream.write([i | 0x80])
-      }
-    }
-  },
-  /**
-   * LEB128 encodeds an interger
-   * @param {String|Number} num
-   * @return {Buffer}
-   */
-  encode: (num) => {
-    const stream = new Stream()
-    module.exports.write(num, stream)
-    return stream.buffer
-  },
-  /**
-   * decodes a LEB128 encoded interger
-   * @param {Buffer} buffer
-   * @return {String}
-   */
-  decode: (buffer) => {
-    const stream = new Stream(buffer)
-    return module.exports.read(stream)
   }
+  return num
+}
+
+function write (number, stream) {
+  const num = new Bn(number)
+  while (true) {
+    const i = num.maskn(7).toNumber()
+    num.ishrn(7)
+    if (num.isZero()) {
+      stream.write([i])
+      break
+    } else {
+      stream.write([i | 0x80])
+    }
+  }
+}
+
+/**
+ * LEB128 encodeds an interger
+ * @param {String|Number} num
+ * @return {Buffer}
+ */
+function encode (num) {
+  const stream = new Stream()
+  write(num, stream)
+  return stream.buffer
+}
+
+/**
+ * decodes a LEB128 encoded interger
+ * @param {Buffer} buffer
+ * @return {String}
+ */
+function decode (buffer) {
+  const stream = new Stream(buffer)
+  return read(stream)
 }
